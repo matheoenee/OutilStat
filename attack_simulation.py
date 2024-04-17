d06774da3b9b564dc6ff394ef2776bd287039a2b
matheo enee <matheoenee@proton.me>
2024-04-17 22:07:58 +0200

    clean code + key rank

---
+++ attack_simulation.py
@@ -0,0 +1,45 @@
+from utilities import *
+from models import Models
+import random
+import numpy as np
+
+# inputs :
+#   method (0 = slice, 1 = space)
+#   loop (number of attack)
+#   n (number of samples)
+#   sigma (standard deviation)
+#   Models (theorical models)
+# output :
+#   average key rank
+
+def attack_simulation(method, loop, n, sigma, Models):
+    kr_sum = 0 # sum of key rank
+    for i in range(loop):
+
+        k = random.randint(0, 255) # random key generation
+
+        lm, ly = generate_leakages(n,sigma,k)
+
+        # space method
+        if method == 0:
+            hm = space_method(lm)
+            hy = space_method(ly)
+        # slice method
+        else:
+            hm = slice_method(lm,n)
+            hy = slice_method(ly,n)
+
+        # generating empirical distribution D
+        D = np.zeros((9, 9),dtype=int)
+        for j in range(n):
+            # Space method
+            HWm = hm[j]
+            HWy = hy[j]
+            D[HWm][HWy] += 1
+        D = D.astype(float)
+        D /= n
+
+        # get key rank
+        kr = get_key_rank(Models,D,k)
+        kr_sum += kr
+    return kr_sum/loop
---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+from utilities import *
+from models import *
+from attack_simulation import *
+
+if __name__ == "__main__":
+    sigma_value = [0.5,0.7,1,1.5]
+    results = {}
+    for sigma in sigma_value:
+        results[sigma] = []
+        results[sigma].append(128)
+        print(f"[sigma = {sigma}]")
+        for n in range(256, 4097, 256):
+            average_kr = attack_simulation(1, 1000, n, sigma, Models)
+            print(f"    [n = {n}] average key rank is : ",average_kr)
+    
+    
